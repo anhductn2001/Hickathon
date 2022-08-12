39191930937e710ba761997b25aef4a9936b5920
truonghust918 <truonghust918@gmail.com>
2022-08-12 15:08:57 +0700

    feat: Add menu for application

---
+++ src/components/Sidebar/SidebarHorizontal.vue
@@ -0,0 +1,56 @@
+<template>
+  <vue-horizontal responsive>
+    <section v-for="item in items" :key="item.title">
+      <div :style="styles.thumbnailWrapper">
+        <comet-chat-avatar
+            border-width="0px"
+            :image="`images/${item.image}.png`"
+        />
+      </div>
+    </section>
+  </vue-horizontal>
+</template>
+
+<script>
+import VueHorizontal from "vue-horizontal";
+import * as style from "@/components/AppNavigation/style";
+
+export default {
+  name: 'component.vue',
+  components: {VueHorizontal},
+  data() {
+    return {
+      // E.g: creates 20 array items...
+      items: [
+        {title: "check", content: "test", image: "support"}
+      ],
+    }
+  },
+  computed: {
+    styles() {
+      return {
+        wrapper: style.wrapperStyle(),
+        title: style.titleStyle(),
+        subTitle: style.subTitleStyle(),
+        helpText: style.helpTextStyle(),
+        component: style.componentStyle(),
+        box: style.boxStyle(),
+        titleWrapper: style.titleWrapperStyle(),
+        thumbnailWrapper: style.thumbnailWrapperStyle(),
+        componentTitle: style.componentTitleStyle(),
+        descWrapper: style.descWrapperStyle(),
+        linkWrapper: style.linkWrapperStyle(),
+        link: style.linkStyle(),
+        logoutBtn: style.logoutBtn(),
+      };
+    },
+  },
+}
+</script>
+
+<style scoped>
+section {
+  padding: 5px 100%;
+  background: #0052CC;
+}
+</style>
